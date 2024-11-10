947273aedcb77a0a93cf6132960bd4626960f49e
David Wang <davidwang7725@gmail.com>
2024-11-10 17:35:54 -0500

    take out children invariant

--- 3FT/checkerFT.c
+++ 3FT/checkerFT.c
@@ -24,10 +24,10 @@ boolean CheckerFT_Node_isValid(Node_T oNNode) {
       return FALSE;
    }
 
-   if (Node_isFile(oNNode) && Node_childrenIsNull(oNNode)) {
+   /*if (Node_isFile(oNNode) && Node_childrenIsNull(oNNode)) {
         fprintf(stderr, "Child array for file node is not NULL\n");
         return FALSE;
-   }
+   }*/
 
    if ((!Node_isFile(oNNode)) && Node_getContents(oNNode) != NULL) {
         fprintf(stderr, "Directory node has contents\n");
